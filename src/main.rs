35ec58b6396ad3aeb227fdb819844999b74f0621
Joe Ardent <code@ardent.nebcorp.com>
2018-05-22 23:26:53 -0700

    Chapter 1, got test ppm image.
    Just prints values to stdout, so redirect to file for display later.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let nx = 400;
+    let ny = 200;
+
+    println!("P3\n{} {}\n255", nx, ny);
+
+    for j in (0..ny).rev() {
+        for i in 0..nx {
+            let r = i as f64 / nx as f64;
+            let g = j as f64 / ny as f64;
+            let b = 0.2;
+
+            let ir = (255.99 * r) as i64;
+            let ig = (255.99 * g) as i64;
+            let ib = (255.99 * b) as i64;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
 }
