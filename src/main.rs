d07291a61586ea9eb249b3504aed700f38523094
Joe Ardent <code@ardent.nebcorp.com>
2018-05-24 21:49:49 -0700

    start of chapter 3

--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,31 @@ impl Point {
     }
 }
 
+impl Ray {
+    pub fn new() -> Self {
+        Ray {
+            a: Default::default(),
+            b: Default::default(),
+        }
+    }
+
+    pub fn init(a: Point, b: Point) -> Self {
+        Ray { a, b }
+    }
+
+    pub fn direction(&self) -> Point {
+        self.b
+    }
+
+    pub fn origin(&self) -> Point {
+        self.a
+    }
+
+    pub fn pt_at_param(&self, t: f64) -> Point {
+        self.a + (self.b * t)
+    }
+}
+
 impl Color {
     pub fn new() -> Self {
         Color { elements: [1.0; 4] }
