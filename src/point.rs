f26b32718263f4134c780006f9058c9de84969cc
Joe Ardent <code@ardent.nebcorp.com>
2018-06-10 13:00:28 -0700

    adds display impl for point

--- src/point.rs
+++ src/point.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use std::ops::{Add, Div, Mul, Sub};
 
 /// Our point is, at heart, an array of four 64-bit floats.
@@ -67,6 +68,12 @@ impl Point {
 }
 
 /// Trait impls here.
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {} {}", self.x(), self.y(), self.z())
+    }
+}
+
 impl Add for Point {
     type Output = Self;
 
