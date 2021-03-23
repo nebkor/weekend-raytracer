c0ce257b453a2e9acbbb3a1da90f4bf898db211c
Joe Ardent <code@ardent.nebcorp.com>
2021-03-23 11:12:28 -0700

    move the Illumable trait into the Ray module

--- src/ray.rs
+++ src/ray.rs
@@ -1,4 +1,4 @@
-use crate::Point3;
+use crate::{Point3, Vec3};
 
 #[derive(Clone, Copy, PartialEq)]
 pub struct Ray {
@@ -22,4 +22,16 @@ impl Ray {
     pub fn at(&self, t: f64) -> Point3 {
         self.origin + (self.dir * t)
     }
+}
+
+// "hit record", though that's a good pun-y name too
+pub struct Glint {
+    pub p: Point3,
+    pub n: Vec3,
+    pub t: f64,
+}
+
+// "hittable"
+pub trait Illumable {
+    fn shine(&self, r: &Ray, t_range: std::ops::Range<f64>) -> Option<Glint>;
 }
